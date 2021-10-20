e22f63cd6f6084bd2bbcc77138ca20de3dc12d73
onno204 <15333546+onno204@users.noreply.github.com>
2021-10-20 16:09:27 +0200

    f

---
+++ FysioWebapp/Controllers/Manage/AgendaController.cs
@@ -0,0 +1,33 @@
+﻿using FysioWebapp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FysioWebapp.Controllers.Manage
+{
+    [Area("Manage")]
+    public class AgendaController : Controller
+    {
+        private readonly ILogger<AgendaController> _logger;
+
+        public AgendaController(ILogger<AgendaController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View("Manage/Agenda/Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
